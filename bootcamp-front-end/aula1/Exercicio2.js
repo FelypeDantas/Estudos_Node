f69f350ff3fee33c448b990e5023e43777c3b409
Felype <felyped03@gmail.com>
2024-12-12 16:11:21 -0300

    Delete bootcamp-front-end/aula1 directory

--- bootcamp-front-end/aula1/Exercicio2.js
+++
@@ -1,9 +0,0 @@
-let precoMedioCombustivel = 4.00;
-let distanciaKm = 1600;
-let consumoVeiculo = 10;
-
-gastoCombustivel = distanciaKm / consumoVeiculo;
-
-let valorDeViagem = precoMedioCombustivel * gastoCombustivel;
-
-console.log("A viagem custou: R$" + valorDeViagem.toFixed(2));
